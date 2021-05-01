6b6e356777095806c6bae1df811f4d4f09ef91e4
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-05-01 14:15:08 +0530

    todo completed

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,4 +1,4 @@
-const todos = [
+let todos = [
     {
         id : Math.random(),
         todo : "complete css clip path",
@@ -27,7 +27,7 @@ function renderTodo(){
         todoItem.classList.add("todo_item");
         const html = `
             <p>${todoitem.todo}</p>
-            <button id=${todoitem.id}>X</button>`
+            <button id=${todoitem.id} onclick=deleteTodo(this.id)>X</button>`
         todoItem.innerHTML = html;
         todosContainer.appendChild(todoItem);
     })
@@ -46,4 +46,15 @@ function addTodo(){
         newTodo.value = "";
         renderTodo();
     }
+}
+
+function deleteTodo(id){
+    let newTodoData = [];
+    todos.map((todo)=>{
+        if(todo.id != id){
+            newTodoData.push(todo);
+        }
+    });
+    todos = newTodoData
+    renderTodo();
 }
