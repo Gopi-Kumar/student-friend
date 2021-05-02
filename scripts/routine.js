1b40b86bed1b2ae701f1ba705b146d28b1a72c8e
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-05-02 22:15:59 +0530

    conditional formatting added to routine js

--- scripts/routine.js
+++ scripts/routine.js
@@ -1,4 +1,4 @@
-let routineItems = []
+let routineItems = getRoutineFromLocalStorage();
 
 
 function saveRoutineToLocalStorage(){
@@ -10,13 +10,13 @@ function getRoutineFromLocalStorage(){
 
 const routineContainer = document.querySelector(".routine");
 function renderRoutine(routineItemArray){
-    if(routineItemArray.length == 0){
+    if(routineItemArray == null || routineItemArray.length == 0 ){
+
         return;
     }
+
     routineContainer.innerHTML = "";
-    if(routineItemArray == null)
-        routineItemsArray = [{}];
-    routineItemArray.map(item => {
+       routineItemArray.map(item => {
         const itemContainer = document.createElement("section");
         itemContainer.classList.add("routine_item");
         const html = `
@@ -34,34 +34,52 @@ renderRoutine(routineItems);
 
 
 function addRoutine(){
-    const fromHr = document.getElementById("fromHr").value;
-    const fromMin = document.getElementById("fromMin").value;
-    const toHr = document.getElementById("toHr").value;
-    const toMin = document.getElementById("toMin").value;
-    const toMeridium = document.getElementById("toMeridium").value;
-    const fromMeridium = document.getElementById("fromMeridium").value;
-    const workName = document.getElementById("workNameInput").value;
-    console.log(fromHr, fromMin, toHr, toMin, toMeridium, fromMeridium, workName);
-    let data = {
-        id : Math.random(),
-        from : {
-            hr : fromHr,
-            min : fromMin,
-            meridium : fromMeridium
-        },
-        to : {
-            hr : toHr,
-            min : toMin,
-            meridium : toMeridium
-        }, 
-        name : workName
-    }
-    
-    routineItems.push(data);
+    let fromHr = document.getElementById("fromHr").value;
+    let fromMin = document.getElementById("fromMin").value;
+    let toHr = document.getElementById("toHr").value;
+    let toMin = document.getElementById("toMin").value;
+    let toMeridium = document.getElementById("toMeridium").value;
+    let fromMeridium = document.getElementById("fromMeridium").value;
+    let workName = document.getElementById("workNameInput").value;
+    if(fromHr && fromHr <= 12 && fromMin && fromMin <= 59 && toHr && toHr <= 12 && toMin && toMin <= 59  && fromMeridium != null && toMeridium != null && workName){
+        if(fromHr < 10){
+            fromHr = `0${fromHr}`
+        }
+        if(fromMin < 10){
+            fromMin = `0${fromMin}`;
+        }
+        if(toHr < 10){
+            toHr = `0${toHr}`
+        }
+        if(toMin < 10){
+            toMin = `0${toMin}`
+        }
+        let data = {
+            id : Math.random(),
+            from : {
+                hr : fromHr,
+                min : fromMin,
+                meridium : fromMeridium
+            },
+            to : {
+                hr : toHr,
+                min : toMin,
+                meridium : toMeridium
+            }, 
+            name : workName
+        }
+        if(routineItems == null){
+            routineItems = [];
+        }
+        routineItems.push(data);
 
-    saveRoutineToLocalStorage();
-    routineItems = getRoutineFromLocalStorage();
-    renderRoutine(routineItems);
+        saveRoutineToLocalStorage();
+        routineItems = getRoutineFromLocalStorage();
+        renderRoutine(routineItems);
+    }else{
+        alert("Fill all field correctly please.");
+    }
+    return;
 
     
 }
