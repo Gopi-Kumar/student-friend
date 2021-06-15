4c80179b2399d4422a2657ee818ad66dd623376b
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-06-16 02:06:11 +0530

    add new note function added

--- scripts/notes.js
+++ scripts/notes.js
@@ -13,13 +13,24 @@ function renderNotes(notesItemArray){
     if(notesItemArray == null || notesItemArray.length == 0){
         return;
     }
+
     notesContainer.innerHtml = "";
+    console.log("rendered")
     notesItemArray.map(item => {
         const itemContainer = document.createElement("div");
+        itemContainer.classList.add("note_container")
         itemContainer.setAttribute("id", `${item.id}`);
+        itemContainer.setAttribute("title", "Click to open")
         const html = `
+            <section class="title">
+                <p class="note_name">${item.name}</p>
+                <div class="option">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </section>
             <p class="note">${item.note}</p>
-            <p class="note_name">${item.name}</p>
         `
         itemContainer.innerHTML = html;
         notesContainer.appendChild(itemContainer);
@@ -30,10 +41,32 @@ renderNotes(notesItem);
 
 const newNoteForm = document.querySelector(".notes_editing");
 
+
 function addNewNote(){
     newNoteForm.style.display = "block";
 }
 function closeNotesEditing(){
     newNoteForm.style.display = "none";
+}
+function saveNote(){
+    const noteText = document.querySelector("textarea").value;
+    const note_name = document.querySelector("#note_name").value;
+    if(noteText == "" || note_name == ""){
+        return;
+    }
+    if(notesItem == null){
+        notesItem = [];
+    }
+
+    notesItem.push({
+        id : Math.random(),
+        name : note_name,
+        note : noteText
+    });
+    saveNotesToLocalStorage();
+    notesItem = getNotesFromLocalStorage();
+    notesContainer.innerHTML = "";
+    renderNotes(notesItem);
+    closeNotesEditing();
 }
 
