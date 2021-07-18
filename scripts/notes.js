716f83bcd24cda16982f21caee3102156112646a
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-07-18 21:30:46 +0530

    note show option added

--- scripts/notes.js
+++ scripts/notes.js
@@ -12,6 +12,26 @@ function getRandomId(){
     return `id_${Math.floor(Math.random() * 10000000)}`;
 }
 
+function showNotes(note, title){
+    let show_note_container = document.querySelector(".show_note");
+    show_note_container.style.display = "flex"
+    let html = `
+        <section>
+            <p>${title}</p>
+            <button onclick="closeShowingNote()">x</button>
+        </section>
+        <section>
+            <p>${note}</p>
+        </section>
+    `
+    show_note_container.innerHTML = html;
+}
+function closeShowingNote(){
+    document.querySelector(".show_note").style.display = "none"
+}
+
+
+
 const notesContainer = document.querySelector(".notes");
 function renderNotes(notesItemArray){
     if(notesItemArray == null || notesItemArray.length == 0){
@@ -26,7 +46,10 @@ function renderNotes(notesItemArray){
         const itemContainer = document.createElement("div");
         itemContainer.classList.add("note_container")
         itemContainer.setAttribute("id", `${item.id}`);
-        itemContainer.setAttribute("title", "Click to open")
+        itemContainer.setAttribute("title", "Double click to open");
+        itemContainer.addEventListener("dblclick", ()=>{
+            showNotes(item.note,item.name);
+        })
         const html = `
             <section class="title">
                 <p class="note_name">${item.name}</p>
@@ -40,7 +63,7 @@ function renderNotes(notesItemArray){
                     <p onclick="deleteNote()">Delete</p>                
                 </div>
             </section>
-            <p class="note">${item.note}</p>
+            <p class="note"">${item.note}</p>
         `
         itemContainer.innerHTML = html;
         notesContainer.appendChild(itemContainer);
@@ -93,6 +116,8 @@ function saveNote(){
 
 let currentNoteId, optionsOpened = false;
 
+
+
 function showOptions(ele){
     if(optionsOpened){
         closeOptions(); 
@@ -124,6 +149,7 @@ function deleteNote(){
     location.reload();
 
 }
+
 
 function editNote(){
     newNoteForm.style.display = "block";
