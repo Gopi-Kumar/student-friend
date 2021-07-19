0a9d01f33c1ecf7c87195fe75b6c103ba146e1ae
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-07-19 14:29:39 +0530

    final commit i think

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,10 +1,13 @@
 let dropdown = document.getElementById("show_dropdown_menu");
+let hideDropdown = document.getElementById("hide_dropdown_menu");
 let navbar = document.querySelector(".header .navbar");
 function showDropdownMenu(){
+    hideDropdown.style.display = "block";
     dropdown.style.display = "none";
     navbar.style.display = "flex";
 }
 function hideDropdownMenu(){
+    hideDropdown.style.display = "none";
     dropdown.style.display = "block";
     navbar.style.display = "none";
 }
